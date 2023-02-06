696b222220ada805724682ad5637b1aef2781c46
RAIM <raim.muslim@gamil.com>
2023-02-06 19:09:42 +0600

    task 2

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int counter = 0;
+        int count = 0;
+        while (counter<=printToInclusive){
+            while (count<=counter){
+                System.out.print(count+" ");
+                count++;
+            }
+            counter++;
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int count = 0;
+        while (power<0){
+            System.out.println("too much power");
+            power=0;
+        }
+        while (count<=power){
+            System.out.println(2<<count);
+            count++;
+        }
     }
 }
